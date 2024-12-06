753b4c796dd2cb53734d77f7a0863db75c161399
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-06 15:54:50 +0000

    First commit for CommandLineCalculator

---
+++ CommandLineCalculator/src/commandlinecalculator/CommandLineCalculator.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package commandlinecalculator;
+import java.util.Scanner;
+
+/**
+ *
+ * @author frederick
+ */
+public class CommandLineCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter mathematical expression: ");
+        Scanner scanner = new Scanner(System.in);
+    }
+    
+}
