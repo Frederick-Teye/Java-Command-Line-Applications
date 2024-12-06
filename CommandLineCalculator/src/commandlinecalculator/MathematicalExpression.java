4486d77a477d9b244d18757b0fbc1a380862497f
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-06 17:21:07 +0000

    Create setRawInput setter

--- CommandLineCalculator/src/commandlinecalculator/MathematicalExpression.java
+++ CommandLineCalculator/src/commandlinecalculator/MathematicalExpression.java
@@ -4,8 +4,12 @@ public class MathematicalExpression {
     private String rawInput;
     
     public MathematicalExpression(String rawInput) {
-        this.rawInput = rawInput; 
+        setRawInput(rawInput); 
     }
     
+    
+    private void setRawInput(String rawInput) {
+        this.rawInput = rawInput;
+    }
     
 }
