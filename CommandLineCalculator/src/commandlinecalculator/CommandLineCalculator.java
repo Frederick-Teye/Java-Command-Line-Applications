daeeb6bd3cfc89024585bf8db4013776b24863bd
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-06 16:05:49 +0000

    Take mathematical expression from user

--- CommandLineCalculator/src/commandlinecalculator/CommandLineCalculator.java
+++ CommandLineCalculator/src/commandlinecalculator/CommandLineCalculator.java
@@ -1,22 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package commandlinecalculator;
 import java.util.Scanner;
 
-/**
- *
- * @author frederick
- */
 public class CommandLineCalculator {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Enter mathematical expression: ");
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter mathematical expression: ");
+        String userInput = scanner.nextLine();
+        
+        
     }
     
 }
