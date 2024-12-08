8853b74f4c2d380a69302248eb6c2b056c588ae9
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-08 23:23:08 +0000

    Create method getBracketsInMathExpression

--- CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
+++ CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
@@ -1,11 +1,29 @@
 package commandlinecalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EvaluateExpression {
 
     private String mathExpression;
 
     public EvaluateExpression(String mathExpression) {
         this.mathExpression = mathExpression;
+    }
+
+
+    private List<Integer> getBracketsInMathExpression() {
+        List<Integer> indexOfAllBracketsInMathExpression = new ArrayList<>();
+
+        for (int i = 0; i < this.mathExpression.length(); i++) {
+
+            if (this.mathExpression.charAt(i) == '('
+                    || this.mathExpression.charAt(i) == ')') {
+                indexOfAllBracketsInMathExpression.add(i);
+            }
+
+        }
+        return indexOfAllBracketsInMathExpression;
     }
 
 
