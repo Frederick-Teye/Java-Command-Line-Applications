8e7e6aa7eea0a053b03562814ca4c4a0685042e1
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-09 05:01:06 +0000

    Add break statement

--- CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
+++ CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
@@ -88,6 +88,7 @@ public class EvaluateExpression {
                     || string.charAt(i) == '-'
                     || i == 0) {
                 endOfOperandOnTheLeft = i;
+                break;
             }
         }
         return endOfOperandOnTheLeft;
