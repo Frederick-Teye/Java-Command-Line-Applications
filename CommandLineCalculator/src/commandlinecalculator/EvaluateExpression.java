de9ad996ed7a8fa0c82056c3fa429b05da51d1a4
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-09 23:04:36 +0000

    Update overseeEvaluation method

--- CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
+++ CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
@@ -12,7 +12,7 @@ public class EvaluateExpression {
     public EvaluateExpression(String mathExpression) {
         this.mathExpression = mathExpression;
         this.indexOfAllBracketsInMathExpression = getBracketsInMathExpression();
-        this.solution = overseeEvaluation();
+        this.solution = overseeEvaluation(this.mathExpression, this.indexOfAllBracketsInMathExpression);
     }
 
 
@@ -97,9 +97,9 @@ public class EvaluateExpression {
     }
 
 
-    private String overseeEvaluation() {
-        String temporalMathExpression = this.mathExpression;
-        List<Integer> temporalIndexOfBrackets = new ArrayList<>(this.indexOfAllBracketsInMathExpression);
+    private String overseeEvaluation(String expression, List<Integer> indexOfBrackets) {
+        String temporalMathExpression = expression;
+        List<Integer> temporalIndexOfBrackets = new ArrayList<>(indexOfBrackets);
 
         if (temporalIndexOfBrackets.isEmpty()) {
             if (numberOfMathOperatorsInString(temporalMathExpression) > 0) {
