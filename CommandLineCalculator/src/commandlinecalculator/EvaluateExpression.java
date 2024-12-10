658cd0b1ce2d8c48728f2bd1ecc7d8ba480b92cf
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-10 09:47:20 +0000

    Create solveDivisionExpression method

--- CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
+++ CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
@@ -186,6 +186,14 @@ public class EvaluateExpression {
     }
 
 
+    private String solveDivisionExpression(String leftOperand, String rightOperand) {
+        double leftOperandCasted = Double.parseDouble(leftOperand);
+        double rightOperandCasted = Double.parseDouble(rightOperand);
+        double result = leftOperandCasted / rightOperandCasted;
+        return Double.toString(result);
+    }
+
+
     @Override
     public String toString() {
         return this.solution;
