59dc4f34714880d24ee49023d85554f5ade7b7b8
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-11 21:27:56 +0000

    Create getTokens() method

--- CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
+++ CommandLineCalculator/src/commandlinecalculator/EvaluateExpression.java
@@ -1,17 +1,44 @@
 package commandlinecalculator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EvaluateExpression {
 
     private String mathExpression;
-    private List<Integer> tokens;
+    private List<String> tokens;
     private String solution;
 
     public EvaluateExpression(String mathExpression) {
         this.mathExpression = mathExpression;
         this.tokens = getTokens();
         this.solution = overseeEvaluation();
+    }
+
+
+    private List<String> getTokens() {
+        List<String> tokens = new ArrayList<>();
+        String currentToken = "";
+        char[] mathExpression = this.mathExpression.toCharArray();
+
+        for (int i = 0; i < mathExpression.length; i++) {
+            char character = mathExpression[i];
+
+            if ('-' == character && (i == 0 
+                    || "+-*/()".contains(Character.toString(mathExpression[i - 1])))) {
+                currentToken += character;
+            } else if ("0123456789.".contains(Character.toString(character))) {
+                currentToken += character;
+            } else {
+                if (!currentToken.isEmpty()) {
+                    tokens.add(currentToken);
+                    currentToken = "";
+                }
+                tokens.add(Character.toString(character));
+            }
+        }
+
+        return tokens;
     }
 
 
