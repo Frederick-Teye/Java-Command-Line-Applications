c7e219ca342d9190934bf3ae34e05b2804a152f2
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-11 14:34:17 +0000

    Update main()

--- CommandLineCalculator/src/commandlinecalculator/CommandLineCalculator.java
+++ CommandLineCalculator/src/commandlinecalculator/CommandLineCalculator.java
@@ -3,24 +3,29 @@ package commandlinecalculator;
 import java.util.Scanner;
 
 public class CommandLineCalculator {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Enter mathematical expression: ");
+
+        System.out.print("Enter mathematical expression: ");
         String userInput = scanner.nextLine().trim();
-        
+
         String mathExpressionWithoutWhiteSpace = userInput.replace(" ", "");
         System.out.println(mathExpressionWithoutWhiteSpace);
-        
-        MathematicalExpression mathExpression = new 
-        MathematicalExpression(mathExpressionWithoutWhiteSpace);
-        
-        String input = mathExpression.toString();
-        
-        EvaluateExpression solution;
-        solution = new EvaluateExpression(input);
-        System.out.println("soution of " + mathExpression + " = " + solution);
+
+        MathematicalExpression mathExpression = new MathematicalExpression(mathExpressionWithoutWhiteSpace);
+
+        if (mathExpression.isRawInputIllegal() == true) {
+            System.out.println("Invalid input, enter only numbers and math operators");
+        } else {
+            String input = mathExpression.toString();
+
+            EvaluateExpression solution;
+            solution = new EvaluateExpression(input);
+            System.out.println("solution of " + mathExpression + " = " + solution);
+        }
+
     }
-    
+
+
 }
