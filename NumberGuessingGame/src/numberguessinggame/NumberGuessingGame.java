095b008bec20c788e239ca4847416da0eb292d84
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-17 17:43:54 +0000

    Welcome the user and take the input from the user

--- NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
+++ NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
@@ -6,6 +6,10 @@ public class NumberGuessingGame {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Welcome!!! A random number between 1 and 100 is \n"
+                + "generated in memory, guess what number it is: ");
+        
+        int userInput = scanner.nextInt();
     }
 
 
