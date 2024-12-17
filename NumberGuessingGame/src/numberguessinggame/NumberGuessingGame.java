05027062e92ec3db7ce3fd6d85627a8011196c1d
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-17 18:15:24 +0000

    Add do while loop to make the game fun

--- NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
+++ NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
@@ -3,7 +3,7 @@ package numberguessinggame;
 import java.util.Scanner;
 
 public class NumberGuessingGame {
-
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Welcome!!! A random number between -1 and 100 is \n"
@@ -11,7 +11,26 @@ public class NumberGuessingGame {
         
         int userInput = scanner.nextInt();
         int generatedRandomNumber = (int) (Math.random() * 100);
+        byte numberOfTrial = 0;
+        
+        do {            
+            if (userInput < generatedRandomNumber) {
+                System.out.println("Your input is too low!\n");
+                numberOfTrial++;
+                System.out.print("Enter a new number: ");
+                userInput = scanner.nextInt();
+            } else if (userInput > generatedRandomNumber) {
+                System.out.println("Your input is too high!\n");
+                numberOfTrial++;
+                System.out.print("Enter a new number: ");
+                userInput = scanner.nextInt();
+            } else {
+                System.out.print("You've won!!!");
+                System.out.println("You've attempted " + numberOfTrial + " times!");
+                break;
+            }
+        } while (true);
     }
-
-
+    
+    
 }
