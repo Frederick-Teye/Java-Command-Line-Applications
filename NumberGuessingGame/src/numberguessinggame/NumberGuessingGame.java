29dff51a566bded1cb6e6eea19df707568217d52
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-17 17:57:04 +0000

    Fix typo

--- NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
+++ NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
@@ -6,7 +6,7 @@ public class NumberGuessingGame {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Welcome!!! A random number between 1 and 100 is \n"
+        System.out.print("Welcome!!! A random number between -1 and 100 is \n"
                 + "generated in memory, guess what number it is: ");
         
         int userInput = scanner.nextInt();
