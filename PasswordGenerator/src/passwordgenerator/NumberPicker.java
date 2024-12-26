3e0e32282b13dfaf0acafb5adf8d35ab0466cc5d
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-26 23:57:58 +0000

    Create NumberPicker and it's numbers field

---
+++ PasswordGenerator/src/passwordgenerator/NumberPicker.java
@@ -0,0 +1,5 @@
+package passwordgenerator;
+
+public class NumberPicker implements RandomCharacterPicker {
+    private final char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+}
