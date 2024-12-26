32af1e25ed40538227445eee7731b518a11dc55f
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-26 23:59:24 +0000

    Implement getRandomCharacter() for NumberPicker

--- PasswordGenerator/src/passwordgenerator/NumberPicker.java
+++ PasswordGenerator/src/passwordgenerator/NumberPicker.java
@@ -1,5 +1,14 @@
 package passwordgenerator;
 
 public class NumberPicker implements RandomCharacterPicker {
-    private final char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+
+    private final char[] numbers = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+
+    @Override
+    public Character getRandomCharacter() {
+        int randomIndex = (int) (Math.random() * numbers.length);
+        return numbers[randomIndex];
+    }
+
+
 }
