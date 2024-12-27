522f63dffe12cae5b15fe861be6e954c5fb2165e
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-27 02:16:05 +0000

    Create fields for GeneratePassword

--- PasswordGenerator/src/passwordgenerator/GeneratePassword.java
+++ PasswordGenerator/src/passwordgenerator/GeneratePassword.java
@@ -2,4 +2,8 @@ package passwordgenerator;
 
 class GeneratePassword {
 
+    private int passwordLength;
+    private int totalNumbers;
+    private int totalCharacters;
+    private int totalLetters;
 }
