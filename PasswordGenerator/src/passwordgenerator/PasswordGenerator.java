b108808d741ba3d0614e5086b66a076e32e6e557
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-27 00:30:42 +0000

    Make PasswordGenerator fields static

--- PasswordGenerator/src/passwordgenerator/PasswordGenerator.java
+++ PasswordGenerator/src/passwordgenerator/PasswordGenerator.java
@@ -2,10 +2,10 @@ package passwordgenerator;
 
 public class PasswordGenerator {
 
-    int passwordLenght;
-    int totalNumbers;
-    int totalCharacters;
-    int totalLetters;
+    static int passwordLenght;
+    static int totalNumbers;
+    static int totalCharacters;
+    static int totalLetters;
 
     public static void main(String[] args) {
         // TODO code application logic here
