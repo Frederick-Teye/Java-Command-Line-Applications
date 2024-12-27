483de555f8d2d6381b1e72fdcbb6cc17b2f380c1
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-27 01:08:55 +0000

    Create isNumeric() method

--- PasswordGenerator/src/passwordgenerator/PasswordGenerator.java
+++ PasswordGenerator/src/passwordgenerator/PasswordGenerator.java
@@ -58,4 +58,18 @@ public class PasswordGenerator {
     }
 
 
+    private static boolean isNumeric(String arg) {
+        if (arg == null) {
+            return false;
+        }
+
+        try {
+            Double.parseDouble(arg);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
