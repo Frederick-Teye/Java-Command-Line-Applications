51597d4e6d5fa88074f8f5adf790d026d1bb58c2
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-27 00:14:49 +0000

    Create getRandomCharacter() method of CharacterPicker

--- PasswordGenerator/src/passwordgenerator/CharacterPicker.java
+++ PasswordGenerator/src/passwordgenerator/CharacterPicker.java
@@ -1,7 +1,16 @@
 package passwordgenerator;
 
 public class CharacterPicker implements RandomCharacterPicker {
+
     public final char[] characters = {'.', ',', ';', ':', '<', '>', '/', ']', '|',
-                              '[', '{', '}', '"', '!', '@', '#', '$', '%', '^','?',
-                              '&', '*', '(', ')', '-', '_', '=', '+', '`', '~'};
+        '[', '{', '}', '"', '!', '@', '#', '$', '%', '^', '?',
+        '&', '*', '(', ')', '-', '_', '=', '+', '`', '~'};
+
+    @Override
+    public Character getRandomCharacter() {
+        int randomIndex = (int) (Math.random() * characters.length);
+        return characters[randomIndex];
+    }
+
+
 }
