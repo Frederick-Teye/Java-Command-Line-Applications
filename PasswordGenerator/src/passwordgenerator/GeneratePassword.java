f26b0456cd6bf13eb7572656426fb477168b42f2
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-27 03:18:56 +0000

    Create makePassword() method of GeneratePassword

--- PasswordGenerator/src/passwordgenerator/GeneratePassword.java
+++ PasswordGenerator/src/passwordgenerator/GeneratePassword.java
@@ -14,6 +14,55 @@ class GeneratePassword {
         this.totalNumbers = totalNumbers;
         this.totalCharacters = totalCharacters;
         this.totalLetters = totalLetters;
+        this.password = makePassword();
+    }
+
+
+    private String makePassword() {
+        char[] passwordArray = new char[passwordLength];
+        RandomCharacterPicker alphabetPicker = new AlphabetPicker();
+        RandomCharacterPicker characterPicker = new CharacterPicker();
+        RandomCharacterPicker numberPicker = new NumberPicker();
+        byte alphabetCounter = 0;
+        byte characterCounter = 0;
+        byte numberCounter = 0;
+        byte currentPasswordSlot = 0;
+        int randomSelector;
+
+        while (true) {
+            randomSelector = (int) (Math.random() * 3) + 1;
+            if (currentPasswordSlot == passwordLength) {
+                break;
+            }
+
+            if (randomSelector == 1) {
+                if (!(alphabetCounter >= totalLetters)) {
+                    passwordArray[currentPasswordSlot] = alphabetPicker.getRandomCharacter();
+                    currentPasswordSlot++;
+                    alphabetCounter++;
+                } else {
+                    continue;
+                }
+            } else if (randomSelector == 2) {
+                if (!(characterCounter >= totalCharacters)) {
+                    passwordArray[currentPasswordSlot] = characterPicker.getRandomCharacter();
+                    currentPasswordSlot++;
+                    characterCounter++;
+                } else {
+                    continue;
+                }
+            } else if (randomSelector == 3) {
+                if (!(numberCounter >= totalCharacters)) {
+                    passwordArray[currentPasswordSlot] = numberPicker.getRandomCharacter();
+                    currentPasswordSlot++;
+                    numberCounter++;
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        return new String(passwordArray);
     }
 
 
