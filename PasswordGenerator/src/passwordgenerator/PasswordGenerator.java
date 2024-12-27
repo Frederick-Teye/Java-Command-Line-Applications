66bbf44beccb1ba15d5f52acceb31c232210da17
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-27 00:27:46 +0000

    Declare PasswordGenerator fields

--- PasswordGenerator/src/passwordgenerator/PasswordGenerator.java
+++ PasswordGenerator/src/passwordgenerator/PasswordGenerator.java
@@ -2,6 +2,11 @@ package passwordgenerator;
 
 public class PasswordGenerator {
 
+    int passwordLenght;
+    int totalNumbers;
+    int totalCharacters;
+    int totalLetters;
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
