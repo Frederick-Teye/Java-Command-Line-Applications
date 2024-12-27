a7c1d76b8edcc126665c556bc00ddb5e70f700ab
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-27 01:20:52 +0000

    Declare fields of CollectInput

--- PasswordGenerator/src/passwordgenerator/CollectInput.java
+++ PasswordGenerator/src/passwordgenerator/CollectInput.java
@@ -2,4 +2,8 @@ package passwordgenerator;
 
 public class CollectInput {
 
+    private int passwordLenght;
+    private int totalNumbers;
+    private int totalCharacters;
+    private int totalLetters;
 }
