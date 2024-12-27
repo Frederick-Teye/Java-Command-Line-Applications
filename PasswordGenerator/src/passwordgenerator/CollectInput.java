3875d7461164a028a47202044ba30ab4d9a10654
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-27 01:32:25 +0000

    Create setNumberOfCharacters() method

--- PasswordGenerator/src/passwordgenerator/CollectInput.java
+++ PasswordGenerator/src/passwordgenerator/CollectInput.java
@@ -13,4 +13,18 @@ public class CollectInput {
         setNumberOfNumbers();
         setPasswordLength();
     }
+
+    private void setNumberOfCharacters() {
+        do {
+            System.out.print("How many characters should password contain: ");
+            String totalCharactersString = scanner.nextLine().trim();
+            if (isNumeric(totalCharactersString)) {
+                totalCharacters = Integer.parseInt(totalCharactersString);
+                System.out.println("");
+                break;
+            } else {
+                System.out.println("Enter numbers only!!!\n");
+            }
+        } while (true);
+    }
 }
