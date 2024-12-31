66e1bf168de0fb88c3a027081ab6746bf5da22b8
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-31 23:56:49 +0000

    Make scanner object static field

--- RockPaperScissors/src/rockpaperscissors/RockPaperScissors.java
+++ RockPaperScissors/src/rockpaperscissors/RockPaperScissors.java
@@ -4,24 +4,27 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Rock Paper Scissors Game");
 
     }
+
 
     private static boolean isNumeric(String arg) {
         if (arg == null) {
             return false;
         }
-        
+
         try {
             Double.parseDouble(arg);
         } catch (Exception e) {
             return false;
         }
-        
+
         return true;
     }
+
 
 }
