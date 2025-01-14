c29cfab7fd12f4f5977fe86deef57d8162f34062
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-01-14 23:49:07 +0000

    Import and initialize a List object

--- WordCount/src/wordcount/WordCount.java
+++ WordCount/src/wordcount/WordCount.java
@@ -1,11 +1,13 @@
 package wordcount;
 
 import java.nio.file.Path;
+import java.util.List;
 
 public class WordCount {
 
     public static void main(String[] args) {
         Path path = Path.of("test.txt");
+        List<String> list = null;
     }
 
 
