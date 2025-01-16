00ab0962f4be3fcd4954df9e253776737301cb51
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-01-16 17:34:24 +0000

    Create two new variables to count words and nonwords

--- WordCount/src/wordcount/WordCount.java
+++ WordCount/src/wordcount/WordCount.java
@@ -10,10 +10,13 @@ public class WordCount {
     public static void main(String[] args) {
         Path path = Path.of("test.txt");
         List<String> lines = null;
-        
+        int numberOfwords = 0;
+        int numberOfNonWords = 0;
+
         try {
             lines = Files.readAllLines(path);
-            String[] linesArray = lines.toArray(new String[] {});
+            String[] linesArray = lines.toArray(new String[]{});
+
         } catch (IOException e) {
             e.printStackTrace();
         }
